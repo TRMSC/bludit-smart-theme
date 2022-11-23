c5e8e404180ce6d0c945e61942123635a1dfe3ee
TRMSC <solidware@web.de>
2022-11-24 00:04:40 +0100

    style navbar

--- php/navbar.php
+++ php/navbar.php
@@ -8,7 +8,7 @@
 		</button>
 		<div class="collapse navbar-collapse" id="navbarResponsive">
 
-			<ul class="navbar-nav ml-auto">
+			<ul class="navbar-nav ml-auto navbar-center">
 
 				<!-- Static pages -->
 				<?php foreach ($staticContent as $staticPage): ?>
