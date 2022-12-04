a2aff1843b66edef9113533153a10a162dec3cf0
TRMSC <solidware@web.de>
2022-12-04 15:14:59 +0100

    change classes

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer bg-dark">
 	<div class="container">
 		<p id="footer-src" class="m-0 text-center text-white text-uppercase"><?php echo $site->footer(); ?>
-			<span class="ml-5 text-warning">
+			<span class="text-warning hold">
 				<img class="mini-logo" src="<?php echo DOMAIN_THEME_IMG.'favicon.png'; ?>"/>
 				Smart theme by <a target="_blank" class="text-white" href="https://github.com/TRMSC">TRMSC</a>
 			</span>
