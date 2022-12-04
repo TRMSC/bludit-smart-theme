83bace3de14624a41602f1ed7f66008930cb0fd0
TRMSC <solidware@web.de>
2022-12-04 15:02:42 +0100

    remove class

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@
 		<p id="footer-src" class="m-0 text-center text-white text-uppercase"><?php echo $site->footer(); ?>
 			<span class="ml-5 text-warning">
 				<img class="mini-logo" src="<?php echo DOMAIN_THEME_IMG.'favicon.png'; ?>"/>
-				Smart theme by <a target="_blank" class="text-white bold" href="https://github.com/TRMSC">TRMSC</a>
+				Smart theme by <a target="_blank" class="text-white" href="https://github.com/TRMSC">TRMSC</a>
 			</span>
 		</p>
 	</div>
