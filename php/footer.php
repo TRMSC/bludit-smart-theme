e77a41c18710bde815d0722944449f5a2edf789a
TRMSC <solidware@web.de>
2022-12-08 20:22:29 +0100

    change sizes

--- php/footer.php
+++ php/footer.php
@@ -1,11 +1,11 @@
 <footer class="footer bg-dark">
 	<div class="container">
-		<div id="footer-src" class="row footer-row text-white text-uppercase">
+		<div id="footer-src" class="row footer-row text-white">
 			<div class="col footer-col footer-left">
 				<?php echo $site->footer(); ?>
 			</div>
 			<div class="col footer-col footer-right">
-				<span class="text-white">Powered by bludit </span>
+				<span class="text-white">Powered by Bludit </span>
 				<img class="mini-logo" src="<?php echo DOMAIN_THEME_IMG.'favicon.png'; ?>"/>
 				<span class="text-warning">Smart theme by</span>&nbsp;
 				<a target="_blank" class="text-warning" href="https://github.com/TRMSC">TRMSC</a>
