5c98805f45d067021a02b501c1b28eef04ae3ec9
TRMSC <solidware@web.de>
2022-12-10 12:31:28 +0100

    add link container

--- php/footer.php
+++ php/footer.php
@@ -9,7 +9,8 @@
 				<img class="mini-logo" src="<?php echo DOMAIN_THEME_IMG.'favicon.png'; ?>"/>
 				<span class="text-warning">Smart theme by</span>&nbsp;
 				<a target="_blank" class="text-warning" href="https://github.com/TRMSC">TRMSC</a>
-			</span>
-		</p>
+			</div>
+			<div id="statimize-links"></div>
+		</div>
 	</div>
 </footer>
