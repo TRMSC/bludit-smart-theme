4b342fb6445331a3f3c9089bde6260cfca9c3ced
TRMSC <solidware@web.de>
2022-12-10 13:09:55 +0100

    revise tree

--- php/footer.php
+++ php/footer.php
@@ -1,16 +1,16 @@
 <footer class="footer bg-dark">
 	<div class="container">
-		<div id="footer-src" class="row footer-row text-white">
-			<div class="col footer-col footer-left">
+		<div id="footer-first-row" class="row footer-row text-white">
+			<div id="footer-text" class="col footer-col footer-left">
 				<?php echo $site->footer(); ?>
 			</div>
-			<div class="col footer-col footer-right">
+			<div id="footer-source" class="col footer-col footer-right">
 				<span class="text-white">Powered by Bludit </span>
 				<img class="mini-logo" src="<?php echo DOMAIN_THEME_IMG.'favicon.png'; ?>"/>
 				<span class="text-warning">Smart theme by</span>&nbsp;
 				<a target="_blank" class="text-warning" href="https://github.com/TRMSC">TRMSC</a>
 			</div>
-			<div id="statimize-links"></div>
+			<div id="footer-links"></div>
 		</div>
 	</div>
 </footer>
