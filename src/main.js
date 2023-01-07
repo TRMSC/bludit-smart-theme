9a22209a9577d7e36e011ba580078b4545fad494
TRMSC <solidware@web.de>
2023-01-07 15:48:17 +0100

    set padding and scroll position

--- src/main.js
+++ src/main.js
@@ -14,13 +14,29 @@ window.onload = () => {
 
 
 /**
- * Set padding for pages body
+ * Set padding for pages body and scroll to new screen position
  * 
+ * @async
  * @function setBodyPadding
+ * @returns {Promise} Scroll to the new screen position
  *
  */
-setBodyPadding = () => {
+setBodyPadding = async () => {
+
+    // Set padding
     const navbarHeight = document.querySelector('.navbar').offsetHeight;
     const footerHeight = document.querySelector('footer').offsetHeight;
-    document.body.style.padding = navbarHeight + 'px 0 ' + (footerHeight + 50) + 'px';
+    document.body.style.padding = navbarHeight + 'px 0 ' + (footerHeight + 50) + 'px 0';
+
+    // Get scroll position
+    const currentScroll = window.scrollY;
+    const newScroll = currentScroll - navbarHeight;
+
+    // Scroll window
+    await new Promise(resolve => setTimeout(resolve, 0));
+    window.scrollTo({
+      top: newScroll,
+      behavior: 'smooth'
+    });
+
 };
